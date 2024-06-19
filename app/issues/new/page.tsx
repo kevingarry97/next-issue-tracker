83fb5f795fd72760ef8f84abbdd2255c6d01f878
kevingarry97 <kevingarry97@gmail.com>
2024-06-20 00:06:03 +0200

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,8 @@ import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -19,6 +21,7 @@ const NewIssuePage = () => {
     resolver:zodResolver(createIssueSchema)
   });
   const [error, setError] = useState('')
+  const [submitting, setSubmitting] = useState(false)
 
   return (
     <div className="max-w-xl ">
@@ -26,14 +29,16 @@ const NewIssuePage = () => {
         <Callout.Text>{error}</Callout.Text></Callout.Root>}
       <form className="space-y-3" onSubmit={handleSubmit(async (data) => {
         try {
+          setSubmitting(true)
           await axios.post('/api/issues', data);
           router.push('/issues')
         } catch (error) {
+          setSubmitting(false)
           setError('An unexpected error occured.');
         }
       })}>
         <TextField.Root placeholder="Title" {...register("title")} />
-        {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+        <ErrorMessage>{errors?.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -41,8 +46,8 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Reply to comment…" {...field} />
           )}
         />
-        {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
-        <Button>Submit New Issue</Button>
+        <ErrorMessage>{errors?.description?.message}</ErrorMessage>
+        <Button disabled={submitting}>Submit New Issue {submitting && <Spinner />} </Button>
       </form>
     </div>
   );
